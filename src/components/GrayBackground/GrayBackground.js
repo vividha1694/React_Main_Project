ed9f3410e1e327ded61d9ec70af9ede4b516cd48
vividha1694 <139147831+vividha1694@users.noreply.github.com>
2024-02-26 16:16:29 +0530

    background

---
+++ src/components/GrayBackground/GrayBackground.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import styles from './GrayBackground.module.css'
+
+const GrayBackground = () => {
+  return (
+    <>
+    <div className={styles.gray}>
+     <h3 className={styles.heading}>Courses</h3>
+    </div>
+    </>
+  )
+}
+
+export default GrayBackground
