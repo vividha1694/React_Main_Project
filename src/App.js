4fd2788d3ff9fda43dc96663948c476912e5f7f5
vividha1694 <139147831+vividha1694@users.noreply.github.com>
2024-02-28 16:21:53 +0530

    render changes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
 
 import './App.css';
+//import Brand from './components/About/Brand';
+//import TestimonialsCard from './components/About/TestimonialsCard';
+//import TestimonialsCardDetails from './components/About/TestimonialsCardDetails';
+//import About from './components/About/About';
+//import DiscoverResearch from './components/About/DiscoverResearch';
+//import TopInstructor from './components/About/TopInstructor';
+//import CourseCardDetails from './components/Courses/CourseCardDetails';
+//import GrayBackground from './components/GrayBackground/GrayBackground';
+//import VideoCard from './components/Home/VideoCard';
 //import Footer from './components/Footer/Footer';
 //import Home from './components/Home/Home';
 //import CourseCard from './components/Home/CourseCard';
-import CourseCardDetails from './components/Home/CourseCardDetails';
+//import CourseCardDetails from './components/Home/CourseCardDetails';
 
 function App() {
   return (
     <div>
-    <CourseCardDetails/>
+     {/* <Brand/> */}
+    {/* <TestimonialsCardDetails/> */}
+    {/* <TopInstructor/> */}
+    {/* <DiscoverResearch/> */}
+    {/* <About/> */}
+    {/* <CourseCardDetails/> */}
+     {/* <GrayBackground/> */}
+    {/* <CourseCardDetails/> */}
+      {/* <VideoCard/> */}
     {/* <Home/> */}
      {/* <Footer/> */}
     </div>
